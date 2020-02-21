179190bd56cae21e8660af98f49a1ca620d49d60
Yasser Ansari <yasser@nsari.co.uk>
2020-02-21 13:38:19 +0000

    Chapter 9 Binding in Classes

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,32 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  // class field
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  // this inside arrow function references a class instance
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
