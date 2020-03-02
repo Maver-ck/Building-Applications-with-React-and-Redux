716ba3e74167ae6bb3fd35a597afb3b2a0ed727d
Maver-ck <yasser.ansari@hotmail.com>
2020-03-02 12:54:27 +0000

    Ch 9 connect container component

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
 class CoursesPage extends React.Component {
   // class field
   state = {
@@ -17,7 +19,11 @@ class CoursesPage extends React.Component {
   handleSubmit = event => {
     // preventDefailt required as the form reloads otherwise and doesn't create the alert
     event.preventDefault();
-    alert(this.state.course.title);
+    // connect function auto adds dispatch as a prop function
+    // we're dispatching the createCourse action and passing it to the course
+    // we HAVE to dispatch an action - if you just call an action creator it wont do anything
+    // action creators just return an object
+    this.props.dispatch(courseActions.createCourse(this.state.course));
   };
 
   render() {
@@ -37,4 +43,17 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage;
+// we expect dispatch to be passed into the component and it will be as connect auto passes in here
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired
+};
+
+// determines what state is passed to our component via props
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+// we omitted mapDispatchToProps => our component gets a dispatch prop injected automatically
+export default connect(mapStateToProps)(CoursesPage);
