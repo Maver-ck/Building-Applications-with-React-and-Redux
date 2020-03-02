49bedd058fe561c45a6d26c48f2f340d148f9aeb
Maver-ck <yasser.ansari@hotmail.com>
2020-03-02 12:28:05 +0000

    Chapter 9 create store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+
+//applyMiddleware - redux middleware is a way to enhance reduc functionality
+
+//reduxImmutableStateInvariant() - this will warn us if we mutate state by mistake (remember the parentheses!)
+
+export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+  );
+}
