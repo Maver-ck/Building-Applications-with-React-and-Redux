daf49590338213e42b7622cbf835d7bbf30f9266
Maver-ck <yasser.ansari@hotmail.com>
2020-03-02 12:34:00 +0000

    Ch 9 instantiate store and provider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
+
+// Provide will provide Redux store data to use our React components
+
+// it can be useful to pass initial stat into the store here
+// if server rendering or initialising redux store with data
+// from local storage
+const store = configureStore();
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
