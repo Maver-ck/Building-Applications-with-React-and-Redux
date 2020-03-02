4f1ada4f304cacd678aa3738c6f8c440cd0454fb
Yasser Ansari <yasser.ansari@hotmail.com>
2020-03-02 11:13:57 +0000

    Chapter 9 - Handle submit

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -8,15 +8,22 @@ class CoursesPage extends React.Component {
     }
   };
 
-  // this inside arrow function references a class instance
+  // "this" inside arrow function references a class instance
   handleChange = event => {
     const course = { ...this.state.course, title: event.target.value };
     this.setState({ course });
   };
 
+  handleSubmit = event => {
+    // preventDefailt required as the form reloads otherwise and doesn't create the alert
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
     return (
-      <form>
+      //this is added here on the form as it'll sumit when you click the enter key
+      <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
         <h3>Add Course</h3>
         <input
