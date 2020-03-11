f99a9dc3d434ea59bc6b099ce5f9ced791bf7379
Maver-ck <yasser.ansari@hotmail.com>
2020-03-11 10:54:46 +0000

    Ch 11 Create Reusable TextInput and SelectInput Components

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,11 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
 
 //function components with hooks are easier to declare and maintain over classes
-function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  ...props
+}) {
+  // useState hook allows us to add React state to function components
+  // initialise course state to a copy of the value passed in on props
+  // NOTE: using react state rather than Redux -
+  // Avoid using Redux for all state.  Use plain React state for local form state and Redux for global values
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
+
   // hook lets us handle side effects
   // this effect will run every time the component renders
   // we want this to run once when the component mounts => we can declare a second argument to useEffect
@@ -27,14 +42,15 @@ function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
   // function component doesn't need "render()" any more as it's implied
   return (
     <>
-      <h2>Manage Course</h2>
+      <CourseForm course={course} errors={errors} authors={authors} />
     </>
   );
 }
 
 // we expect dispatch to be passed into the component and it will be as connect auto passes in here
 ManageCoursePage.propTypes = {
-  actions: PropTypes.object.isRequired,
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired
@@ -43,6 +59,7 @@ ManageCoursePage.propTypes = {
 // determines what state is passed to our component via props
 function mapStateToProps(state) {
   return {
+    course: newCourse,
     courses: state.courses,
     authors: state.authors
   };
